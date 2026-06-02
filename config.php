36578ce55e8e84d480191d1510cfa162b9381eb2
pohlmary <iftixortoychoyev1@gmail.com>
2026-06-02 20:00:46 +0500

    Fix WEBAPP_URL trailing slash to prevent 301 redirect stripping Telegram data

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 define('BOT_TOKEN', '8854883342:AAFBlJwYy3Ev0_3owiGj2SrdcY64oHJ7B9k');
 define('GROUP_CHAT_ID', '-5284614531');
-define('WEBAPP_URL', 'https://iftixorgo.bigsaver.ru/webapp');
+define('WEBAPP_URL', 'https://iftixorgo.bigsaver.ru/webapp/');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'iftixorgo');
 define('DB_USER', 'bulungur');
