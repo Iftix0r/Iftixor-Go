4d7a46e80151949b3e3e6973f3a0be7c8c9f960c
pohlmary <iftixortoychoyev1@gmail.com>
2026-06-03 14:56:36 +0500

    23ds

--- config.php
+++ config.php
@@ -6,6 +6,7 @@ define('REST_BOT_TOKEN', '8775488113:AAFwHS-U9toEBXIxSNx2LngHgmc5gbMbSSI');
 define('REST_WEBAPP_URL', 'https://iftixorgo.bigsaver.ru/restaurant_webapp/index.html');
 define('ADMIN_URL', 'https://iftixorgo.bigsaver.ru/admin');
 define('ADMIN_IDS', [2114098498]); // Telegram ID larini qo'shing, masalan: [123456789, 987654321]
+define('ADMIN_BOT_TOKEN', '8960721267:AAGbiv5ZYm2hCDddyIL0Qx2rKHFb0QQAF6I');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'iftixorgo');
 define('DB_USER', 'bulungur');
