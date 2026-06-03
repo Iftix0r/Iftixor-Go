c933a97a36e5ef553b90f8eb89df3869564a1a16
pohlmary <iftixortoychoyev1@gmail.com>
2026-06-03 14:20:15 +0500

    chore: update REST_WEBAPP_URL path and add trailing whitespace to config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ define('BOT_TOKEN', '8854883342:AAFBlJwYy3Ev0_3owiGj2SrdcY64oHJ7B9k');
 define('GROUP_CHAT_ID', '-5284614531');
 define('WEBAPP_URL', 'https://iftixorgo.bigsaver.ru/webapp/');
 define('REST_BOT_TOKEN', '8775488113:AAFwHS-U9toEBXIxSNx2LngHgmc5gbMbSSI');
-define('REST_WEBAPP_URL', 'https://iftixorgo.bigsaver.ru/restaurant_webapp/');
+define('REST_WEBAPP_URL', 'https://iftixorgo.bigsaver.ru/restaurant_webapp/index.html');
 define('ADMIN_URL', 'https://iftixorgo.bigsaver.ru/admin');
 define('ADMIN_IDS', [2114098498]); // Telegram ID larini qo'shing, masalan: [123456789, 987654321]
 define('DB_HOST', 'localhost');
@@ -12,3 +12,4 @@ define('DB_USER', 'bulungur');
 define('DB_PASS', 'Iftixor2006');
 define('CURRENCY', 'so\'m');
 define('DELIVERY_FEE', 5000);
+ 
